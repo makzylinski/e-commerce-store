d108fcd9a2cc516eb54787631feb8a241ea0a0af
Maks <makzylinski@gmail.com>
2026-09-22 15:41:11 +0200

    multiple changes

--- src/main/java/com/max/spring_e_commerce/controller/ProductController.java
+++ src/main/java/com/max/spring_e_commerce/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -26,4 +28,17 @@ public class ProductController {
     public ResponseEntity<Product> getProduct(@PathVariable int productId) {
         return new ResponseEntity<>(productService.getProduct(productId), HttpStatus.OK);
     }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, image);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
--- src/main/java/com/max/spring_e_commerce/service/ProductService.java
+++ src/main/java/com/max/spring_e_commerce/service/ProductService.java
@@ -4,7 +4,9 @@ import com.max.spring_e_commerce.model.Product;
 import com.max.spring_e_commerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,5 +21,12 @@ public class ProductService {
 
     public Product getProduct(int productId) {
         return productRepository.findById(productId).orElse(new Product());
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepository.save(product);
     }
 }
