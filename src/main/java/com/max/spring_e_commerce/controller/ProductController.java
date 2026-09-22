78794f772e98807cbab51d17c9ca7d5cf3709ad6
Maks <makzylinski@gmail.com>
2026-09-22 14:51:09 +0200

    changes

--- src/main/java/com/max/spring_e_commerce/controller/ProductController.java
+++ src/main/java/com/max/spring_e_commerce/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.max.spring_e_commerce.controller;
 import com.max.spring_e_commerce.model.Product;
 import com.max.spring_e_commerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +18,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/product/{productId}")
