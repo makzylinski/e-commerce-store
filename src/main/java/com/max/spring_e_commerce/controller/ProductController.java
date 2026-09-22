eab62df4f8c61bb8d25534807004050117c030b8
Maks <makzylinski@gmail.com>
2026-09-22 21:12:32 +0200

    ep

--- src/main/java/com/max/spring_e_commerce/controller/ProductController.java
+++ src/main/java/com/max/spring_e_commerce/controller/ProductController.java
@@ -30,10 +30,10 @@ public class ProductController {
     }
 
     @PostMapping("/product")
-    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
         Product savedProduct = null;
         try {
-            savedProduct = productService.addProduct(product, image);
+            savedProduct = productService.addProduct(product, imageFile);
             return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
         } catch (IOException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
