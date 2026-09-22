d3fa38feff467d69fb6efa0dd5d19410b48cff02
Maks <makzylinski@gmail.com>
2026-09-22 14:40:12 +0200

    cross origin

--- src/main/java/com/max/spring_e_commerce/controller/ProductController.java
+++ src/main/java/com/max/spring_e_commerce/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.max.spring_e_commerce.controller;
 import com.max.spring_e_commerce.model.Product;
 import com.max.spring_e_commerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
     @Autowired
