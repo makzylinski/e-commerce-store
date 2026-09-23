3a0ce09a4b61041073953b1bf54819cca864eed5
Maks <makzylinski@gmail.com>
2026-09-23 12:20:24 +0200

    interface

--- src/main/java/com/max/spring_e_commerce/repository/OrderRepository.java
+++ src/main/java/com/max/spring_e_commerce/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.max.spring_e_commerce.repository;
 
+import com.max.spring_e_commerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
