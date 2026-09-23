aa790a21e5a9bca67f6f367b10ff8204088ba626
Maks <makzylinski@gmail.com>
2026-09-23 12:21:03 +0200

    repo

--- src/main/java/com/max/spring_e_commerce/repository/OrderRepository.java
+++ src/main/java/com/max/spring_e_commerce/repository/OrderRepository.java
@@ -4,6 +4,9 @@ import com.max.spring_e_commerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Optional<Order> findByOrderId(String orderId);
 }
