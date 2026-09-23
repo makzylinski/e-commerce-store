dd1bed099bfb3960621e2dfa57b7371f54361415
Maks <makzylinski@gmail.com>
2026-09-23 12:18:16 +0200

    order controller and service

---
+++ src/main/java/com/max/spring_e_commerce/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.max.spring_e_commerce.service;
+
+import com.max.spring_e_commerce.model.dto.OrderRequest;
+import com.max.spring_e_commerce.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
